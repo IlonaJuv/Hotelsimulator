86fbab3ad469e971c377de2e67a07a185ef38b08
IlonaJuv <99474935+IlonaJuv@users.noreply.github.com>
2022-09-15 16:17:10 +0300

    Changing events names (enum).

--- src/simu/model/OmaMoottori.java
+++ src/simu/model/OmaMoottori.java
@@ -15,9 +15,9 @@ public class OmaMoottori extends Moottori{
 			
 		palvelupisteet = new Palvelupiste[3];
 	
-		palvelupisteet[0]=new Palvelupiste(new Normal(10,6), tapahtumalista, TapahtumanTyyppi.DEP1);	
-		palvelupisteet[1]=new Palvelupiste(new Normal(10,10), tapahtumalista, TapahtumanTyyppi.DEP2);
-		palvelupisteet[2]=new Palvelupiste(new Normal(5,3), tapahtumalista, TapahtumanTyyppi.DEP3);
+		palvelupisteet[0]=new Palvelupiste(new Normal(10,6), tapahtumalista, TapahtumanTyyppi.BAARISTAPOISTUMINEN);
+		palvelupisteet[1]=new Palvelupiste(new Normal(10,10), tapahtumalista, TapahtumanTyyppi.POISTUMINENPALVELUTISKI1);
+		palvelupisteet[2]=new Palvelupiste(new Normal(5,3), tapahtumalista, TapahtumanTyyppi.RAVINTOLASTAPOISTUMINEN);
 		
 		saapumisprosessi = new Saapumisprosessi(new Negexp(15,5), tapahtumalista, TapahtumanTyyppi.ARR1);
 
@@ -37,13 +37,13 @@ public class OmaMoottori extends Moottori{
 			case ARR1: palvelupisteet[0].lisaaJonoon(new Asiakas());	
 				       saapumisprosessi.generoiSeuraava();	
 				break;
-			case DEP1: a = palvelupisteet[0].otaJonosta();
+			case BAARISTAPOISTUMINEN: a = palvelupisteet[0].otaJonosta();
 				   	   palvelupisteet[1].lisaaJonoon(a);
 				break;
-			case DEP2: a = palvelupisteet[1].otaJonosta();
+			case POISTUMINENPALVELUTISKI1: a = palvelupisteet[1].otaJonosta();
 				   	   palvelupisteet[2].lisaaJonoon(a); 
 				break;  
-			case DEP3: 
+			case RAVINTOLASTAPOISTUMINEN:
 				       a = palvelupisteet[2].otaJonosta();
 					   a.setPoistumisaika(Kello.getInstance().getAika());
 			           a.raportti(); 
